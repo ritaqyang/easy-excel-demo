48cb70428a2e599d3fcd1be3164fded891a1e4df
ritaqyang <ritaqyang@gmail.com>
2024-07-30 08:04:01 -0400

    easy excel demo

---
+++ src/main/java/com/rita/data/PersonData.java
@@ -0,0 +1,15 @@
+package com.rita.data;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+public class PersonData {
+    private String name;
+    private Integer age;
+    private String email;
+
+}
